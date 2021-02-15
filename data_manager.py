4f7f3ca33955978a0ff75b6a77d9f0a198424f99
dandrea1 <47754826+dandrea1@users.noreply.github.com>
2021-02-15 14:43:37 -0500

    Create data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,26 @@
+import requests
+
+SHEETY_ENDPOINT = "YOUR_ENDPOINT_HERE"
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT)
+        data = response.json()
+        print(data)
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
